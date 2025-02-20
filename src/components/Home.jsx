0fe0de2c13837e2d6b0af6c40b7d057694826cc3
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-02-20 10:34:36 +0530

    home-page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import '../styles/Home.css';
 import home from '../assets/home.png';
 
 const Home = () => {
   return (
-    <div>
-        <div>
+    <div className="home">
+        <div className='home-content'>
         <h2>Hello,</h2>
         <h1>I'm Mohanapriyan</h1>
-        <p>I'm Full Stack Developer</p>
-        <div>
-            <button>Let's Talk</button>
-            <button>Download CV</button>
+        <p>I'm Full Stack Developer <b>|</b></p>
+        <div className='btns'>
+            <button className='btn-1'>Let's Talk</button>
+            <button className='btn-2'>Download CV</button>
         </div>
         </div>
-        <img src={home} alt='landing img'/>
+        <img className='home-img' src={home} alt='landing img'/>
     </div>
   )
 }
