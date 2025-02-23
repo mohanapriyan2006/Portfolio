d90d5084ab1a6953f4cdcb93fe278a587307bf69
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-02-23 14:37:06 +0530

    css-v7

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
     <div className="home">
       <div className='home-content'>
         <h2>Hello,</h2>
-        <h1>I'm Mohanapriyan</h1>
-        <p>I'm Full Stack Developer <b>|</b></p>
+        <h1>I'm <b style={{color: '#0084ff'}}>Mohanapriyan</b></h1>
+        <div className='typing-container'>Full Stact Developer !</div>
         <div className='btns'>
           <a style={{ textDecoration: 'none', color: 'white' }} href='#contact'><button className='btn-1'>Let's Talk</button></a>
           <a style={{ textDecoration: 'none', color: 'white' }} href={resume} target='new'><button className='btn-2'>Download CV</button></a>
