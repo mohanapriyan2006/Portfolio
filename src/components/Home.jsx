14e247c74fdca2e5ad4fb982de759c01aa7bcba7
mohanapriyan2006 <mohanapriyanyt@gmail.com>
2025-02-25 14:21:18 +0530

    responsive-v7

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className='home-content'>
         <h2>Hello,</h2>
         <h1>I'm <b style={{color: '#0084ff'}}>Mohanapriyan</b></h1>
-        <div className='typing-container'>Full Stact Developer !</div>
+        <div className='typing-container'>Full Stack Developer </div>
         <div className='btns'>
           <a style={{ textDecoration: 'none', color: 'white' }} href='#contact'><button className='btn-1'>Let's Talk</button></a>
           <a style={{ textDecoration: 'none', color: 'white' }} href={resume} target='new'><button className='btn-2'>Download CV</button></a>
